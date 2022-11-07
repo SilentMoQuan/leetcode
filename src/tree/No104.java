141eb18b442c8565da61d1c9f79ec0455d356d17
moquan <moquanmail@163.com>
2022-11-07 17:59:35 +0800

    No111 No104

---
+++ src/tree/No104.java
@@ -0,0 +1,45 @@
+package tree;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * describe
+ * <br />
+ *
+ * @author :<b> wangYuanHong </b><br />
+ * @date :<b> 2022/11/7 17:51 </b><br />
+ */
+public class No104 {
+
+    public int maxDepth(TreeNode root) {
+
+        int deep = 0;
+        if(root == null){
+            return deep;
+        }
+
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.addLast(root);
+        while (!queue.isEmpty()){
+
+            int size = queue.size();
+            deep++;
+            for (int i = 0; i < size; i++) {
+
+                TreeNode node = queue.pollFirst();
+
+                if (null != node.left) queue.addLast(node.left);
+                if (null != node.right) queue.addLast(node.right);
+
+
+            }
+
+
+        }
+
+        return deep;
+
+    }
+
+}
