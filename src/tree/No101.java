391002e348b57e84d90f73f45abc1c4de3a53172
moquan <moquanmail@163.com>
2022-11-11 11:29:33 +0800

    No101 其他写法

--- src/tree/No101.java
+++ src/tree/No101.java
@@ -16,8 +16,34 @@ public class No101 {
 
     public boolean isSymmetric(TreeNode root) {
 
+        if(null == root ){
+            return true;
+        }
+
+        return compare(root.left, root.right);
+    }
+
+    private boolean compare(TreeNode left, TreeNode right) {
+
+        if (left == null && right != null){
+            return false;
+        }else if(left != null && right == null){
+            return false;
+        }else if(left == null && right == null){
+            return true;
+        }else if(left.val != right.val){
+            return false;
+        }
+
+        boolean compareOut = compare(left.left, right.right);
+        boolean compareInner = compare(left.right, right.left);
+
+        return compareOut && compareInner;
+    }
+
+    public boolean isSymmetricOld(TreeNode root) {
+
         Deque<TreeNode> queue = new ArrayDeque<>();
-        TreeNode NullNode = new TreeNode(101);
         queue.addLast(root);
 
         while (!queue.isEmpty()) {
