f6e630c0e5cef4e43d653a31b0f85052909106bb
MoQuan <moquanmail@163.com>
2022-11-17 23:51:21 +0800

    N106 end

--- src/tree/No106.java
+++ src/tree/No106.java
@@ -1,5 +1,10 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * describe
  * <br />
@@ -9,9 +14,39 @@ package tree;
  */
 public class No106 {
 
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
+    public static void main(String[] args) {
 
-        return null;
+        No106 no106 = new No106();
+        no106.buildTree(new int[]{9,3,15,20,7}, new int[]{9,15,7,20,3});
+    }
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return grow(inorder, 0, inorder.length - 1,  postorder, 0, postorder.length - 1);
+    }
+
+    public TreeNode grow(int[] inorder, int inBegin, int inEnd,  int[] postOrder, int postBegin, int postEnd){
+        
+        if (postEnd < postBegin || inBegin > inEnd){
+            return null;
+        }
+
+        Integer rootVal = postOrder[postEnd];
+        TreeNode node = new TreeNode(rootVal);
+
+        int leftLen = 0;
+        for (int i = inBegin; i <= inEnd; i++) {
+
+            if (inorder[i] == rootVal){
+                leftLen = i - inBegin;
+                break;
+            }
+
+        }
+
+        node.left = grow(inorder, inBegin, inBegin + leftLen - 1, postOrder, postBegin, postBegin + leftLen - 1);
+        node.right = grow(inorder, inBegin + leftLen + 1, inEnd, postOrder, postBegin + leftLen, postEnd - 1);
+        
+        return node;
     }
 
 }
