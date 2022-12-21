aa0412c46ff9f62a588cc97a7c2256658b55dec1
moquan <moquanmail@163.com>
2022-12-21 16:09:39 +0800

    no406

---
+++ src/greedy/No406.java
@@ -0,0 +1,33 @@
+package greedy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+/**
+ * describe
+ * <br />
+ *
+ * @author :<b> wangYuanHong </b><br />
+ * @date :<b> 2022/12/21 15:39 </b><br />
+ */
+public class No406 {
+
+    public int[][] reconstructQueue(int[][] people) {
+
+        Arrays.sort(people, (a, b) -> {
+            if (a[0] == b[0])
+                return a[1] - b[1];
+            return b[0] - a[0];
+        });
+
+        LinkedList<int[]> que = new LinkedList<>();
+
+        for (int[] p : people) {
+            que.add(p[1],p);
+        }
+
+        return que.toArray(new int[people.length][]);
+    }
+
+}
